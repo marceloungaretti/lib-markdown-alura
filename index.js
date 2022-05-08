30eab3bdc23365240af427ee5dfd63fcc1491b87
marceloungaretti <marcelo.ungaretti@gmail.com>
2022-05-08 12:07:31 -0300

    fim do capitulo 1

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
+const paragrafo = 'Texto retornado por uma função';
+
+function texto(string){
+    return string;
+}
+
+console.log(texto(paragrafo));
