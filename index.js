cb6d5380d29c27305070e31fa2577979b16f96ec
marceloungaretti <marcelo.ungaretti@gmail.com>
2022-05-10 17:04:31 -0300

    fim do capitulo 3

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-import chalk from 'chalk';
-import fs from 'fs';
+import chalk from "chalk";
+import fs from "fs";
 
-function trataErro(erro){
-    throw new Error(chalk.red(erro.code, 'Arquivo não encontrado.'));
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({
+      [temp[1]]: temp[2]
+    });
+  }
+
+  return arrayResultados;
 }
 
-async function pegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8';
-    try {
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, "Arquivo não encontrado."));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = "utf-8";
+  try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.green(texto))
-    } catch(erro) {
-        trataErro(erro);
-    }
+    console.log(extraiLinks(texto));
+  } catch (erro) {
+    trataErro(erro);
+  }
 }
 
-pegaArquivo('./arquivos/texto1.md');
+pegaArquivo("./arquivos/texto1.md");
