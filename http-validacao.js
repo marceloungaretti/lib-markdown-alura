7284569136419e2529e839bb4766b444ee3da1f3
marceloungaretti <marcelo.ungaretti@gmail.com>
2022-06-06 00:25:48 -0300

    fim do capitulo 5

--- http-validacao.js
+++ http-validacao.js
@@ -1,9 +1,35 @@
-function geraArrayDeUrls(arrayLinks){
-    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+import fetch from "node-fetch";
+
+function manejaErros(erro) {
+  throw new Error(erro.message);
 }
 
-function validaURLs(arrayLinks){
-    return geraArrayDeUrls(arrayLinks);
+async function checaStatus(arrayURLs) {
+  try {
+    const arrayStatus = await Promise.all(
+      arrayURLs.map(async (url) => {
+        const res = await fetch(url);
+        return res.status;
+      })
+    );
+    return arrayStatus;
+  } catch (erro) {
+    manejaErros(erro);
+  }
+}
+
+function geraArrayDeUrls(arrayLinks) {
+  return arrayLinks.map((objetoLink) => Object.values(objetoLink).join());
+}
+
+async function validaURLs(arrayLinks) {
+  const links = geraArrayDeUrls(arrayLinks);
+  const statusLinks = await checaStatus(links);
+  const resultados = arrayLinks.map((objeto, indice) => ({
+    ...objeto,
+    status: statusLinks[indice],
+  }));
+  return resultados;
 }
 
 export default validaURLs;
